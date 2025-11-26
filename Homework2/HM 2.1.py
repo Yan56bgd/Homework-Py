56245cfcb481b398a1888b1f8ae2e79b2863f31a
Ivan <zz@mail.ru>
2025-11-26 09:46:37 +0300

    update

--- Homework2/HM 2.1.py
+++ Homework2/HM 2.1.py
@@ -1,13 +1,20 @@
-# 1. Минимальное, максимальное значение и медиана
-def task1(sample):
-    sorted_sample = sorted(sample)
-    n = len(sorted_sample)
-    min_val = sorted_sample[0]
-    max_val = sorted_sample[-1]
+def calculate_statistics(numbers):
+    sorted_numbers = sorted(numbers)
+    n = len(sorted_numbers)
+    
+    min_value = sorted_numbers[0]
+    max_value = sorted_numbers[-1]
     
     if n % 2 == 1:
-        median = sorted_sample[n // 2]
+        median = sorted_numbers[n // 2]
     else:
-        median = (sorted_sample[n // 2 - 1] + sorted_sample[n // 2]) / 2
+        median = (sorted_numbers[n // 2 - 1] + sorted_numbers[n // 2]) / 2
     
-    return min_val, max_val, median
+    return min_value, max_value, median
+
+numbers = [12, 5, 8, 15, 3, 9, 7, 10, 6]
+min_val, max_val, median = calculate_statistics(numbers)
+
+print(f"Минимальное: {min_val}")
+print(f"Максимальное: {max_val}")
+print(f"Медиана: {median}")
--- Homework2/HM 2.2.py
+++ Homework2/HM 2.2.py
@@ -1,13 +1,32 @@
-# 2. Гистограмма и вероятности
-def task2():
-    import random
-    data = [random.randint(0, 100) for _ in range(20)]
-    
-    histogram = [0] * 10
-    for num in data:
-        bin_index = num // 10
-        histogram[bin_index] += 1
-    
-    probabilities = [count / len(data) for count in histogram]
-    
-    return data, histogram, probabilities
+import random
+
+data = [random.randint(0, 100) for _ in range(20)]
+
+histogram = [0] * 10
+probabilities = [0] * 10
+
+for value in data:
+    bin_index = value // 10
+    if bin_index > 9:
+        bin_index = 9
+    histogram[bin_index] += 1
+
+for i in range(10):
+    probabilities[i] = histogram[i] / len(data)
+
+print("Список данных:", data)
+print("\nГистограмма:")
+for i in range(10):
+    start = i * 10
+    end = start + 9
+    if i == 9:
+        end = 100
+    print(f"[{start:2}-{end:3}]: {histogram[i]:2} элементов")
+
+print("\nВероятности:")
+for i in range(10):
+    start = i * 10
+    end = start + 9
+    if i == 9:
+        end = 100
+    print(f"[{start:2}-{end:3}]: {probabilities[i]:.2f}")
